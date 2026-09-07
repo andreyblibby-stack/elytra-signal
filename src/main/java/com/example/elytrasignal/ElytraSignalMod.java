ba6ad4a007d7b4d9d3970233908725ff8e3e2854
andreyblibby-stack <andreyblibby@gmail.com>
2026-09-07 12:55:37 +0700

    Update ElytraSignalMod.java  import

--- src/main/java/com/example/elytrasignal/ElytraSignalMod.java
+++ src/main/java/com/example/elytrasignal/ElytraSignalMod.java
@@ -2,7 +2,7 @@ package com.example.elytrasignal;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.util.Identifier;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
